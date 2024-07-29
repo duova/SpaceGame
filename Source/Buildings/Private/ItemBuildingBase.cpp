313e74b3df3db961c7a3dc5a2c4c2b4a8dd69d6b
Kai Chang <rezonabyt@gmail.com>
2024-07-30 01:48:58 +0800

    Update to last commit.

--- Source/Buildings/Private/ItemBuildingBase.cpp
+++ Source/Buildings/Private/ItemBuildingBase.cpp
@@ -129,6 +129,8 @@ bool AItemBuildingBase::TryFlush()
 
 	for (UInventoryComponent* InvComp : NearbyInventories)
 	{
+		const TSubclassOf<UItem> Filter = Cast<AWarehouseBuildingBase>(InvComp->GetOwner())->Filter;
+		
 		TArray<FGameplayTag> InventoryIds;
 
 		for (const FInventory& Inventory : InvComp->Inventories)
@@ -141,6 +143,7 @@ bool AItemBuildingBase::TryFlush()
 			TArray<UItem*>& Items = Buffer->GetInventory(FGameplayTag())->Items;
 			for (uint16 i = 0; i < Items.Num(); i++)
 			{
+				if (Filter != nullptr && Filter != Items[i]->GetClass()) continue;
 				if (Items[i]->GetClass() == UItem::StaticClass()) continue;
 				InvComp->MoveItemAnySlot(InvComp, Id, FGameplayTag(), i);
 			}
